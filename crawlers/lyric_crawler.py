c3342999a2be0f509d0f95d04f353a91cea55f06
arnold samuel Chan <arnold.chan@lndata.com>
2020-03-20 23:10:19 +0800

    docstring: added explanation

--- crawlers/lyric_crawler.py
+++ crawlers/lyric_crawler.py
@@ -14,7 +14,16 @@ from crawlers.util.db import ORMBaseClass, engine
 # ORMBaseClass.metadata.drop_all(bind=engine)
 # ORMBaseClass.metadata.create_all(bind=engine)
 class LyricCrawler(CrawlerBase):
-    def __init__(self,artist_name,song_title):
+    """Crawling Lyric from www.genius.com
+    Default input from Spotify's data
+    However, if the crawler couldn't find the default lyric,
+    it would fetch lyric from the first search result
+    in genius.
+    Parameter:
+    :artist_name: singer name
+    :song_title: title of the song"""
+
+    def __init__(self,artist_name,song_title)    :
         self.artist_name = artist_name
         self.song_title = song_title
         self.album = None
@@ -27,6 +36,7 @@ class LyricCrawler(CrawlerBase):
 
     @property
     def lyric_url(self):
+        """generate the default form of lyric's link"""
         def combine_with_dash(list_,first_cap=True):
             def remove_symbol(word,symbols):
                 for symbol in symbols:
@@ -48,6 +58,7 @@ class LyricCrawler(CrawlerBase):
     
     @property
     def fallback_url(self):
+        """if the default url couldn't be found, it'll use search result"""
         combine = self.song_title + self.artist_name
         return unidecode("https://genius.com/search?q="+quote(combine))
 
@@ -65,14 +76,25 @@ class LyricCrawler(CrawlerBase):
                 raise "URL NOT FOUND"
 
     def run(self):
+        """get, and save the information in self.result
+        
+        Returns:
+            [self] -- [LyricCrawler object]
+        """        
+        #do the request
         self.get_soup()
+
         paragraph = self.soup.find('div',class_="lyrics").find('p')
         [ads.extract() for ads in paragraph.find_all('defer-compile')];
+
+        # saving lyric and album
         self.lyric = paragraph.text.strip().replace('\u2005',' ')
         self.album= self.soup.find(text="Album").parent.nextSibling.nextSibling.text.strip()
         
         content_info = self.soup.find('div',{"initial-content-for":"track_info"})
         writer_soup = content_info.find(text="Written By")
+
+        # saving song writer
         if writer_soup is not None:
             writers = writer_soup.parent.nextSibling.nextSibling.text.strip()
             self.writer = []
@@ -81,25 +103,24 @@ class LyricCrawler(CrawlerBase):
         else:
             print(f'writer not found for: {self.artist_name}-{self.song_title}')
         
+        # save song's distributor
         distributor_soup = content_info.find(text="Distributor")
         if distributor_soup is not None:
             self.distributor = distributor_soup.parent.nextSibling.nextSibling.text.strip()
         else:
             print(f'distributor not found for: {self.artist_name}-{self.song_title}')
-        
+        # save song's label
         label_soup = content_info.find(text="Label")
         if label_soup is not None:
             self.label = label_soup.parent.nextSibling.nextSibling.text.strip()
         else:
             print(f'label not found for: {self.artist_name}-{self.song_title}')
-        
+        # save song's release date
         release_date_soup = content_info.find(text="Release Date")
         if release_date_soup is not None:
             self.release_date = dateparser.parse(release_date_soup.parent.nextSibling.nextSibling.text.strip())
-            # self.song_obj.release_date = release_date
         else:
             print(f'release date not found for: {self.artist_name}-{self.song_title}')
-        # self.commit()
         self.result = {"artist_name": self.artist_name,
                 "song_title": self.song_title,
                 "album": self.album,
@@ -113,6 +134,13 @@ class LyricCrawler(CrawlerBase):
         return self
 
     def to_db(self,song_obj, artist_obj, db_session):
+        """Saving information to database
+        
+        Arguments:
+            song_obj {[Song]} -- [needed to update the song object]
+            artist_obj {[Artist]} -- [needed to update the artist object]
+            db_session {[mysqlalchemy.session]} -- [SQL session engine connector]
+        """        
         self.session = db_session
         # write lyric
         new_lyric = Lyric(song_obj.id,self.lyric)
@@ -140,7 +168,6 @@ class LyricCrawler(CrawlerBase):
             new_distributor = Distributor(self.distributor)
             new_distributor = self.session.merge(new_distributor)
             self.session.commit()
-        
 
         # label
         if self.label is not None:
@@ -155,6 +182,3 @@ class LyricCrawler(CrawlerBase):
             song_obj.distributor_id = new_distributor.id
         song_obj.release_date = self.release_date
         self.session.commit()
-
-    # def commit(self):
-    #     self._commit(self.session)
